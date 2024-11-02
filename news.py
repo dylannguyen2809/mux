0c309d8d94416e6120e5b68f459067d521c7d0a6
Joyce <jh829@duke.edu>
2024-11-02 19:10:28 -0400

    News api

---
+++ news.py
@@ -0,0 +1,30 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+def give_news(user_input):
+    load_dotenv()
+    NEWS_KEY = os.getenv("NEWS_KEY")
+    url = ('https://newsapi.org/v2/top-headlines?'
+        'q='+str(user_input)+'&'
+        'sortBy=popularity&'
+        'pageSize=5&'
+        'apiKey='+str(NEWS_KEY))
+
+    response = requests.get(url)
+    json = response.json()
+    news = []
+        
+    for article in json["articles"]:
+        if article['source']['id'] != None:
+            article_info = {
+                "title": article.get("title"),
+                "author": article.get("author"),
+                "description": article.get("description"),
+                "published_date": article.get("publishedAt"),
+                "content": article.get("content")
+            }
+            news.append(article_info)
+    return news
+
+give_news('Tesla')
